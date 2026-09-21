b5648f69efc418976fde0170211dbdeaf8e5d9a6
meatsu <meatsu92@gmail.com>
2026-09-21 11:26:18 +0800

    added mongodb connection status

--- database/db.js
+++ database/db.js
@@ -2,6 +2,12 @@ const mongoose = require("mongoose")
 
 const connect = async () => {
     try{
+
+        console.log(
+            "MONGODB_URI exists:",
+            !!process.env.MONGODB_URI
+        )
+
         const connection = await mongoose.connect(process.env.MONGODB_URI)
 
         console.log("Connected to:", connection.connection.name)
